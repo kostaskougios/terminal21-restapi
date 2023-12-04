7d415d219020a460f0e68c9a270973b71401ffac
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-04 18:45:49 +0000

    -

--- examples/src/main/scala/examples/HelloWorld.scala
+++ examples/src/main/scala/examples/HelloWorld.scala
@@ -2,6 +2,9 @@ package examples
 
 import org.terminal21.client.Sessions
 
+import java.util.UUID
+
 @main def helloWorld(): Unit =
-  Sessions.withNewSession("hello-world", "Hello World"): session =>
+  val r = UUID.randomUUID().toString.substring(0, 4)
+  Sessions.withNewSession(s"hello-world-$r", s"Hello World $r"): session =>
     println(s"Session $session created")
