e2b7df9e8512557d7d7f6da44b1ab1abdad331b9
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-19 18:37:46 +0000

    -

--- terminal21-server/src/main/scala/org/terminal21/server/service/CommandWebSocket.scala
+++ terminal21-server/src/main/scala/org/terminal21/server/service/CommandWebSocket.scala
@@ -8,30 +8,7 @@ import io.circe.syntax.*
 import io.helidon.common.buffers.BufferData
 import org.slf4j.LoggerFactory
 import org.terminal21.model.{ClientToServer, CommandEvent, SubscribeTo}
-import org.terminal21.server.ui.WsSessionOps
 import org.terminal21.ws.{ReliableServerWsListener, ServerValue}
-
-//class CommandWebSocket(sessionsService: ServerSessionsService) extends WsListener:
-//  private val logger      = LoggerFactory.getLogger(getClass.getName)
-//  private val errorLogger = new ErrorLogger(logger)
-//
-//  override def onOpen(wsSession: WsSession): Unit =
-//    errorLogger.logErrors:
-//      logger.info(s"Session $wsSession opened")
-//      wsSession.send("init", true)
-//
-//  override def onClose(wsSession: WsSession, status: Int, reason: String): Unit =
-//    logger.info(s"Session $wsSession closed")
-//
-//  override def onMessage(wsSession: WsSession, text: String, last: Boolean): Unit =
-//    errorLogger.logErrors:
-//      decode[ClientToServer](text) match
-//        case Right(SubscribeTo(session)) =>
-//          logger.info(s"Command subscribes to events of session ${session.id}")
-//          sessionsService.notifyMeOnSessionEvents(session): event =>
-//            WsSessionOps.returnTrueIfSessionIsNotClosed:
-//              wsSession.send(event.asJson.noSpaces, true)
-//        case Left(e)                     => throw new IllegalStateException(s"Invalid json : json = $text  error = $e")
 
 class CommandWebSocket(fiberExecutor: FiberExecutor, sessionsService: ServerSessionsService):
   private val logger = LoggerFactory.getLogger(getClass.getName)
@@ -64,9 +41,9 @@ class CommandWebSocket(fiberExecutor: FiberExecutor, sessionsService: ServerSess
           case SubscribeTo(session) =>
             logger.info(s"Command subscribes to events of session ${session.id}")
             sessionsService.notifyMeOnSessionEvents(session): event =>
-              WsSessionOps.returnTrueIfSessionIsNotClosed:
-                val e = ServerValue(sv.id, event)
-                send(e)
+              val e = ServerValue(sv.id, event)
+              send(e)
+              true
 
 trait CommandWebSocketBeans:
   def sessionsService: ServerSessionsService
