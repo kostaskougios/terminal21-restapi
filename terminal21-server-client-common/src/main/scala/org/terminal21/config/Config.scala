5f082a3b9d9cc9d5911c5d5c24fc24b901840c22
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-21 17:22:58 +0000

    -

--- terminal21-server-client-common/src/main/scala/org/terminal21/config/Config.scala
+++ terminal21-server-client-common/src/main/scala/org/terminal21/config/Config.scala
@@ -3,4 +3,7 @@ package org.terminal21.config
 case class Config(host: String, port: Int)
 
 object Config:
-  val Default = Config("localhost", 8080)
+  private def host = sys.env.getOrElse("TERMINAL21_HOST", "localhost")
+  private def port = sys.env.getOrElse("TERMINAL21_PORT", "8080").toInt
+
+  val Default = Config(host, port)
--- terminal21-server/src/main/scala/org/terminal21/server/Terminal21Server.scala
+++ terminal21-server/src/main/scala/org/terminal21/server/Terminal21Server.scala
@@ -4,28 +4,33 @@ import functions.fibers.FiberExecutor
 import io.helidon.logging.common.LogConfig
 import io.helidon.webserver.WebServer
 import io.helidon.webserver.http.HttpRouting
+import org.slf4j.LoggerFactory
 import org.terminal21.config.Config
 
-@main def terminal21Server(): Unit =
-  FiberExecutor.withFiberExecutor: executor =>
-    val config        = Config.Default
-    val dependencies  = new Dependencies(executor)
-    val routesBuilder = HttpRouting.builder()
-    Routes.register(dependencies, routesBuilder)
-    Routes.static(routesBuilder)
+object Terminal21Server:
+  private val logger                        = LoggerFactory.getLogger(getClass)
+  def start(port: Option[Int] = None): Unit =
+    FiberExecutor.withFiberExecutor: executor =>
+      val portV         = port.getOrElse(Config.Default.port)
+      val dependencies  = new Dependencies(executor)
+      val routesBuilder = HttpRouting.builder()
+      Routes.register(dependencies, routesBuilder)
+      Routes.static(routesBuilder)
 
-    LogConfig.configureRuntime()
-    val server = WebServer.builder
-      .port(config.port)
-      .routing(routesBuilder)
-      .addRouting(Routes.ws(dependencies))
-      .build
-      .start
-    try
-      println(s"Terminal 21 Server started and listening on http://localhost:${config.port}")
-      while true do
-        Thread.sleep(86400 * 1000)
-        println("One more day passed...")
-    finally
-      println("Server stopping....")
-      server.stop()
+      LogConfig.configureRuntime()
+      val server = WebServer.builder
+        .port(portV)
+        .routing(routesBuilder)
+        .addRouting(Routes.ws(dependencies))
+        .build
+        .start
+      try
+        logger.info(s"Terminal 21 Server started and listening on http://localhost:$portV")
+        while true do
+          Thread.sleep(86400 * 1000)
+          logger.info("One more day passed...")
+      finally
+        logger.info("Server stopping....")
+        server.stop()
+
+@main def terminal21ServerMain(): Unit = Terminal21Server.start()
