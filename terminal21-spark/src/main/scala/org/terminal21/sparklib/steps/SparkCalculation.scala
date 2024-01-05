0d7ce82cf16f37d3f4ec79db17e14d72f56231fb
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-05 21:16:17 +0000

    -

--- terminal21-spark/src/main/scala/org/terminal21/sparklib/steps/SparkCalculation.scala
+++ terminal21-spark/src/main/scala/org/terminal21/sparklib/steps/SparkCalculation.scala
@@ -1,6 +1,7 @@
 package org.terminal21.sparklib.steps
 
 import functions.fibers.FiberExecutor
+import org.apache.spark.sql.Dataset
 import org.terminal21.client.components.UiElement.HasStyle
 import org.terminal21.client.{Calculation, ConnectedSession}
 import org.terminal21.client.components.chakra.{Badge, Box, Button, HStack, RepeatIcon, Text}
@@ -9,16 +10,16 @@ import org.terminal21.client.components.{Keys, UiComponent, UiElement}
 abstract class SparkCalculation[IN, OUT](
     val key: String = Keys.nextKey,
     @volatile var children: Seq[UiElement],
-    notifyWhenCalcReady: Seq[Calculation[OUT, _]]
+    notifyWhenCalcReady: Seq[Calculation[Dataset[OUT], _]]
 )(using executor: FiberExecutor)
-    extends Calculation[IN, OUT](notifyWhenCalcReady)
+    extends Calculation[IN, Dataset[OUT]](notifyWhenCalcReady)
     with UiComponent
 
 abstract class StdSparkCalculation[IN, OUT](
     key: String = Keys.nextKey,
     name: String,
     dataUi: UiElement with HasStyle,
-    notifyWhenCalcReady: Seq[Calculation[OUT, _]]
+    notifyWhenCalcReady: Seq[Calculation[Dataset[OUT], _]]
 )(using session: ConnectedSession, executor: FiberExecutor)
     extends SparkCalculation[IN, OUT](key, Nil, notifyWhenCalcReady):
   val badge  = Badge()
@@ -49,7 +50,7 @@ abstract class StdSparkCalculation[IN, OUT](
     session.render()
     super.whenResultsNotReady()
 
-  override protected def whenResultsReady(results: OUT): Unit =
+  override protected def whenResultsReady(results: Dataset[OUT]): Unit =
     badge.text = "Ready"
     badge.colorScheme = None
     recalc.isDisabled = Some(false)
@@ -57,18 +58,19 @@ abstract class StdSparkCalculation[IN, OUT](
     session.render()
 
 object SparkCalculation:
-  class Builder[IN, OUT](name: String, dataUi: UiElement with HasStyle, notifyWhenCalcReady: Seq[Calculation[OUT, _]], calc: IN => OUT)(using
+  class Builder[IN, OUT](name: String, dataUi: UiElement with HasStyle, notifyWhenCalcReady: Seq[Calculation[Dataset[OUT], _]], calc: IN => Dataset[OUT])(using
       session: ConnectedSession,
       executor: FiberExecutor
   ):
-    def whenResultsReady(ready: OUT => Unit) =
+    def whenResultsReady(ready: Dataset[OUT] => Unit) =
       new StdSparkCalculation[IN, OUT](name = name, dataUi = dataUi, notifyWhenCalcReady):
-        override protected def calculation(in: IN): OUT             = calc(in)
-        override protected def whenResultsReady(results: OUT): Unit =
+        override protected def calculation(in: IN): Dataset[OUT]             = calc(in)
+        override protected def whenResultsReady(results: Dataset[OUT]): Unit =
           ready(results)
           super.whenResultsReady(results)
 
-  def stdSparkCalculation[IN, OUT](name: String, dataUi: UiElement with HasStyle, notifyWhenCalcReady: Calculation[OUT, _]*)(calc: IN => OUT)(using
+  def stdSparkCalculation[IN, OUT](name: String, dataUi: UiElement with HasStyle, notifyWhenCalcReady: Calculation[Dataset[OUT], _]*)(calc: IN => Dataset[OUT])(
+      using
       session: ConnectedSession,
       executor: FiberExecutor
   ) =
--- terminal21-spark/src/test/scala/org/terminal21/sparklib/steps/StdSparkCalculationTest.scala
+++ terminal21-spark/src/test/scala/org/terminal21/sparklib/steps/StdSparkCalculationTest.scala
@@ -1,39 +1,51 @@
 package org.terminal21.sparklib.steps
 
+import org.apache.spark.sql.Dataset
 import org.scalatest.concurrent.Eventually.eventually
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 import org.terminal21.client.components.chakra.*
 import org.terminal21.client.{ConnectedSession, ConnectedSessionMock, given}
+import org.terminal21.sparklib.SparkSessions
 
 import java.util.concurrent.atomic.AtomicBoolean
+import scala.util.Using
 
 class StdSparkCalculationTest extends AnyFunSuiteLike:
+  test("calculates the correct result"):
+    Using.resource(SparkSessions.newSparkSession()): spark =>
+      import spark.implicits.*
+      given ConnectedSession = ConnectedSessionMock.newConnectedSessionMock
+
+      val calc = new StdSparkCalculation[Int, Int]("key", "name", Box(), Nil):
+        override protected def calculation(in: Int) = Seq(in + 1).toDS
+
+      calc.run(1).collect().toList should be(List(2))
+
   test("whenResultsNotReady"):
-    given ConnectedSession = ConnectedSessionMock.newConnectedSessionMock
-    val dataUi             = Box()
-    val called             = new AtomicBoolean(false)
-    val calc               = new StdSparkCalculation[Int, Int]("key", "name", dataUi, Nil):
-      override protected def calculation(in: Int) = in + 1
+    Using.resource(SparkSessions.newSparkSession()): spark =>
+      import spark.implicits.*
+      given ConnectedSession = ConnectedSessionMock.newConnectedSessionMock
+      val called             = new AtomicBoolean(false)
+      val calc               = new StdSparkCalculation[Int, Int]("key", "name", Box(), Nil):
+        override protected def calculation(in: Int)        = Seq(in + 1).toDS
+        override protected def whenResultsNotReady(): Unit =
+          called.set(true)
 
-      override protected def whenResultsNotReady(): Unit =
-        called.set(true)
-        super.whenResultsNotReady()
-
-    calc.run(1)
-    called.get() should be(true)
+      calc.run(1)
+      called.get() should be(true)
 
   test("whenResultsReady"):
-    given ConnectedSession = ConnectedSessionMock.newConnectedSessionMock
-    val dataUi             = Box()
-    val called             = new AtomicBoolean(false)
-    val calc               = new StdSparkCalculation[Int, Int]("key", "name", dataUi, Nil):
-      override protected def calculation(in: Int) = in + 1
+    Using.resource(SparkSessions.newSparkSession()): spark =>
+      import spark.implicits.*
+      given ConnectedSession = ConnectedSessionMock.newConnectedSessionMock
+      val called             = new AtomicBoolean(false)
+      val calc               = new StdSparkCalculation[Int, Int]("key", "name", Box(), Nil):
+        override protected def calculation(in: Int)                          = Seq(in + 1).toDS()
+        override protected def whenResultsReady(results: Dataset[Int]): Unit =
+          results.collect().toList should be(List(2))
+          called.set(true)
 
-      override protected def whenResultsReady(results: Int): Unit =
-        called.set(true)
-        super.whenResultsReady(results)
-
-    calc.run(1)
-    eventually:
-      called.get() should be(true)
+      calc.run(1)
+      eventually:
+        called.get() should be(true)
