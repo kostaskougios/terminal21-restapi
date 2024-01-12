ca99aea15b6e13d1deb5d97eb1bf5cd47ad4b5ab
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-12 16:26:32 +0000

    -

--- end-to-end-tests/src/main/scala/tests/MathJaxComponents.scala
+++ end-to-end-tests/src/main/scala/tests/MathJaxComponents.scala
@@ -13,7 +13,7 @@ import org.terminal21.client.components.mathjax.*
         Text(text = "Lets write some math expressions that will wow everybody!"),
         MathJax(expression = """\[\sum_{n = 200}^{1000}\left(\frac{20\sqrt{n}}{n}\right)\]""")
       ),
-      MathJax(expression = """Everyone knows this one : \(ax^2 + bx + c = 0\)"""),
+      MathJax(expression = """Everyone knows this one : \(ax^2 + bx + c = 0\). But how about this? \(sum_(i=1)^n i^3=((n(n+1))/2)^2\)"""),
       MathJax(
         expression = """Does it align correctly? \(ax^2 + bx + c = 0\) It does provided CHTML renderer is used.""",
         style = Map("backgroundColor" -> "gray")
--- terminal21-mathjax/src/main/scala/org/terminal21/client/components/mathjax/MathJax.scala
+++ terminal21-mathjax/src/main/scala/org/terminal21/client/components/mathjax/MathJax.scala
@@ -5,9 +5,13 @@ import org.terminal21.client.components.{Keys, UiElement}
 
 sealed trait MathJaxElement extends UiElement
 
+/** see https://asciimath.org/
+  */
 case class MathJax(
     key: String = Keys.nextKey,
-    @volatile var expression: String = "\\[\\sum_{n = 100}^{1000}\\left(\\frac{10\\sqrt{n}}{n}\\right)\\]",
+    /** expression should be like """ text \( asciimath \) text""", i.e. """When \(a \ne 0\), there are two solutions to \(ax^2 + bx + c = 0\)"""
+      */
+    @volatile var expression: String = """fill in the expression as per https://asciimath.org/""",
     @volatile var style: Map[String, Any] = Map.empty // Note: some of the styles are ignored by mathjax lib
 ) extends MathJaxElement
     with HasStyle
