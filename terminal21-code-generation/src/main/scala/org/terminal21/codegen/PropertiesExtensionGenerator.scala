4ff02626a508b71961d575dedaab08f77b9c4d0a
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-17 14:27:21 +0000

    -

--- terminal21-code-generation/src/main/scala/org/terminal21/codegen/PropertiesExtensionGenerator.scala
+++ terminal21-code-generation/src/main/scala/org/terminal21/codegen/PropertiesExtensionGenerator.scala
@@ -8,33 +8,37 @@ import java.io.File
 object PropertiesExtensionGenerator:
   private val e = StructureExtractor()
 
-  def extract(tasty: String): Seq[Code] =
-    val packages = e.fromFiles(List(tasty))
-    packages.map: p =>
-      val ext = p.types
+  def extract(tastys: List[String]): Code =
+    val packages = e.fromFiles(tastys)
+    val ext      = packages.map: p =>
+      val extCode = p.types
         .filterNot(_.name.contains("$"))
         .map: t =>
           createExtension(t)
 
-      Code(
-        "-",
-        s"""
+      extCode.mkString("\n")
+
+    val p = packages.head
+    Code(
+      s"${p.name.replace('.', '/')}/extensions.scala",
+      s"""
          |package ${p.name}
-         |${ext.mkString("\n")}""".stripMargin
-      )
+         |${ext.mkString("\n")}
+         |""".stripMargin
+    )
 
   def createExtension(t: EType): String =
     val methods = t.vals.map: vl =>
       s"def ${vl.name}(v: ${vl.`type`.simplifiedCode}) = e.copy(${vl.name} = v)"
 
     s"""
-       |extension (e: Paragraph)
+       |extension (e: ${t.name})
        |  ${methods.mkString("\n  ")}
        |""".stripMargin
 
 @main def propertiesExtensionGeneratorApp(): Unit =
   val targetDir  = new File("../terminal21-ui-std/target/")
   val scala3Dir  = targetDir.listFiles().find(_.getName.startsWith("scala-3")).get
-  val classesDir = new File(scala3Dir, "classes")
-  val codes       = extract(classesDir.getAbsolutePath + "/org/terminal21/client/components/std/Paragraph.tasty")
-  println(codes.mkString("\n"))
+  val classesDir = new File(scala3Dir, "classes/org/terminal21/client/components/std/")
+  val code       = extract(classesDir.listFiles().filter(_.getName.endsWith(".tasty")).filterNot(_.getName.contains("$")).map(_.getAbsolutePath).toList)
+  println(code)
