b1da80c6b5fa28f1900ad78ab80e6d749ab09550
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-18 20:02:34 +0000

    -

--- terminal21-ui-std/src/main/scala/org/terminal21/client/components/UiComponent.scala
+++ terminal21-ui-std/src/main/scala/org/terminal21/client/components/UiComponent.scala
@@ -5,7 +5,8 @@ import org.terminal21.client.components.UiElement.HasChildren
 /** A UiComponent is a UI element that is composed of a seq of other ui elements
   */
 trait UiComponent extends UiElement:
-  // Note: impl as a lazy val to avoid UiElements getting a random key
+  // Note: impl as a lazy val to avoid UiElements getting a random key and try to fix the
+  // keys of any sub-elements the component has.
   def rendered: Seq[UiElement]
   override def flat = Seq(this) ++ rendered.flatMap(_.flat)
 
