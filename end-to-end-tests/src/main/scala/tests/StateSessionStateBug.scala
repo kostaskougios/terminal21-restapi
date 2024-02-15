f302c04ba42bcd43d5edea045fc773e0030f77b8
Kostas Kougios <kostas.kougios@googlemail.com>
2024-02-15 14:15:06 +0000

    -

---
+++ end-to-end-tests/src/main/scala/tests/StateSessionStateBug.scala
@@ -0,0 +1,45 @@
+package tests
+
+import org.terminal21.client.components.*
+import org.terminal21.client.components.chakra.*
+import org.terminal21.client.components.std.Paragraph
+import org.terminal21.client.{ConnectedSession, Sessions}
+
+import java.util.Date
+
+@main def stateSessionStateBug(): Unit =
+  Sessions
+    .withNewSession("state-session", "Stale Session")
+    .connect: session =>
+      given ConnectedSession = session
+
+      var exitFlag = false
+      val date     = new Date()
+      Seq(
+        Paragraph(text = s"Now: $date"),
+        QuickTable()
+          .headers("Title", "Value")
+          .rows(
+            Seq(
+              Seq(
+                "Date - Editable",
+                Editable(defaultValue = date.toString)
+                  .withChildren(
+                    EditablePreview(),
+                    EditableInput()
+                  )
+              ),
+              Seq(
+                "Date - Input",
+                Input(value = date.toString)
+              ),
+              Seq(
+                "Date - Std Input",
+                std.Input(defaultValue = Some(date.toString))
+              )
+            )
+          ),
+        Button(text = "Close").onClick: () =>
+          exitFlag = true
+      ).render()
+      session.waitTillUserClosesSessionOr(exitFlag)
