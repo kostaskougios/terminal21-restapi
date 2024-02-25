3e9f6c5a21c3244d2002dbaddf3176a7e28e55fe
Kostas Kougios <kostas.kougios@googlemail.com>
2024-02-25 16:56:48 +0000

    -

--- terminal21-ui-std/src/main/scala/org/terminal21/client/components/std/StdElement.scala
+++ terminal21-ui-std/src/main/scala/org/terminal21/client/components/std/StdElement.scala
@@ -5,57 +5,57 @@ import org.terminal21.client.components.UiElement.{HasChildren, HasStyle}
 import org.terminal21.client.components.{Keys, OnChangeEventHandler, UiElement}
 import org.terminal21.collections.TypedMap
 
-sealed trait StdEJson                   extends UiElement
-sealed trait StdElement[A <: UiElement] extends StdEJson with HasStyle
+sealed trait StdEJson   extends UiElement
+sealed trait StdElement extends StdEJson with HasStyle
 
-case class Span(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Span]:
+case class Span(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Span
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class NewLine(key: String = Keys.nextKey, style: Map[String, Any] = Map.empty) extends StdElement[NewLine]:
+case class NewLine(key: String = Keys.nextKey, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = NewLine
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
 
-case class Em(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Em]:
+case class Em(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Em
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header1(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header1]:
+case class Header1(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header1
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header2(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header2]:
+case class Header2(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header2
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header3(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header3]:
+case class Header3(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header3
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header4(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header4]:
+case class Header4(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header4
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header5(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header5]:
+case class Header5(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header5
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
   def withText(v: String)                     = copy(text = v)
 
-case class Header6(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement[Header6]:
+case class Header6(key: String = Keys.nextKey, text: String, style: Map[String, Any] = Map.empty) extends StdElement:
   type This = Header6
   override def withStyle(v: Map[String, Any]) = copy(style = v)
   def withKey(v: String)                      = copy(key = v)
@@ -66,7 +66,7 @@ case class Paragraph(
     text: String = "",
     style: Map[String, Any] = Map.empty,
     children: Seq[UiElement] = Nil
-) extends StdElement[Paragraph]
+) extends StdElement
     with HasChildren:
   type This = Paragraph
   override def withChildren(cn: UiElement*)   = copy(children = cn)
@@ -81,7 +81,7 @@ case class Input(
     style: Map[String, Any] = Map.empty,
     valueReceived: Option[String] = None, // use value instead
     dataStore: TypedMap = TypedMap.empty
-) extends StdElement[Input]
+) extends StdElement
     with CanHandleOnChangeEvent:
   type This = Input
   override def defaultEventHandler                = newValue => copy(valueReceived = Some(newValue))
