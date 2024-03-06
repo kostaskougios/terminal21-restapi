d194dfec4f5387d22d53c099fc3749601ff24a26
Kostas Kougios <kostas.kougios@googlemail.com>
2024-03-06 18:08:12 +0000

    -

--- terminal21-server/src/test/scala/org/terminal21/server/service/ServerSessionsServiceTest.scala
+++ terminal21-server/src/test/scala/org/terminal21/server/service/ServerSessionsServiceTest.scala
@@ -1,6 +1,5 @@
 package org.terminal21.server.service
 
-import io.circe.Json
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 import org.terminal21.model.{OnChange, OnClick, SessionClosed, SessionOptions}
@@ -103,40 +102,6 @@ class ServerSessionsServiceTest extends AnyFunSuiteLike:
 
       createSession()
       listenerCalled should be(2)
-
-  test("changeSessionJsonState changes session's state"):
-    ???
-//    new App:
-//      val session = createSession()
-//      val sj1     = serverJson(elements = Map("e1" -> Json.fromString("e1v")))
-//      serverSessionsService.setSessionJsonState(session, sj1)
-//      val sj2     = serverJson(elements = Map("e2" -> Json.fromString("e2v")))
-//      serverSessionsService.changeSessionJsonState(session, sj2)
-//      serverSessionsService.sessionStateOf(session).serverJson should be(
-//        sj1.include(sj2)
-//      )
-
-  test("changeSessionJsonState notifies listeners"):
-    ???
-//    new App:
-//      val session = createSession()
-//      val sj1     = serverJson(elements = Map("e1" -> Json.fromString("e1v")))
-//      serverSessionsService.setSessionJsonState(session, sj1)
-//      val sj2     = serverJson(elements = Map("e2" -> Json.fromString("e2v")))
-//      var called  = 0
-//      serverSessionsService.notifyMeWhenSessionChanges: (s, sessionState, sjOption) =>
-//        called match
-//          case 0 =>
-//            s should be(session)
-//            sjOption should be(None)
-//          case 1 =>
-//            s should be(session)
-//            sjOption should be(Some(sj2))
-//
-//        called += 1
-//        true
-//      serverSessionsService.changeSessionJsonState(session, sj2)
-//      called should be(2)
 
   test("triggerUiEvent notifies listeners for clicks"):
     new App:
--- terminal21-ui-std/src/test/scala/org/terminal21/client/ConnectedSessionTest.scala
+++ terminal21-ui-std/src/test/scala/org/terminal21/client/ConnectedSessionTest.scala
@@ -5,9 +5,9 @@ import org.mockito.Mockito.verify
 import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers.*
 import org.terminal21.client.ConnectedSessionMock.encoder
-import org.terminal21.client.components.chakra.{Box, Button, Checkbox, Editable, Input}
+import org.terminal21.client.components.chakra.Editable
 import org.terminal21.client.components.std.{Paragraph, Span}
-import org.terminal21.model.{CommandEvent, OnChange}
+import org.terminal21.model.OnChange
 import org.terminal21.ui.std.ServerJson
 
 class ConnectedSessionTest extends AnyFunSuiteLike:
@@ -29,43 +29,13 @@ class ConnectedSessionTest extends AnyFunSuiteLike:
     )
 
   test("to server json"):
-    ???
-//    val (sessionService, connectedSession) = ConnectedSessionMock.newConnectedSessionAndSessionServiceMock
-//
-//    val p1    = Paragraph(key = "pk", text = "p1")
-//    val span1 = Span(key = "sk", text = "span1")
-//    connectedSession.render(Seq(p1.withChildren(span1)))
-//    connectedSession.render(Nil)
-//    verify(sessionService).setSessionJsonState(
-//      connectedSession.session,
-//      ServerJson(
-//        Seq("root"),
-//        Map(
-//          "root"    -> encoder(Box("root")).deepDropNullValues,
-//          p1.key    -> encoder(p1.withChildren()).deepDropNullValues,
-//          span1.key -> encoder(span1).deepDropNullValues
-//        ),
-//        Map("root"  -> List(p1.key), p1.key -> Seq(span1.key), span1.key -> Nil)
-//      )
-//    )
-
-  test("renderChanges changes state on server"):
-    ???
-//    val (sessionService, connectedSession) = ConnectedSessionMock.newConnectedSessionAndSessionServiceMock
-//
-//    val p1    = Paragraph(key = "pk", text = "p1")
-//    val span1 = Span(key = "sk", text = "span1")
-//    connectedSession.render(Seq(p1))
-//    connectedSession.renderChanges(Seq(p1.withChildren(span1)))
-//    verify(sessionService).changeSessionJsonState(
-//      connectedSession.session,
-//      ServerJson(
-//        Seq("root"),
-//        Map(
-//          "root"    -> encoder(Box("root")).deepDropNullValues,
-//          p1.key    -> encoder(p1.withChildren()).deepDropNullValues,
-//          span1.key -> encoder(span1).deepDropNullValues
-//        ),
-//        Map("root"  -> List(p1.key), p1.key -> Seq(span1.key), span1.key -> Nil)
-//      )
-//    )
+    val (sessionService, connectedSession) = ConnectedSessionMock.newConnectedSessionAndSessionServiceMock
+    val span1                              = Span("sk", text = "span1")
+    val p1                                 = Paragraph("pk", text = "p1").withChildren(span1)
+    connectedSession.render(Seq(p1.withChildren(span1)))
+    verify(sessionService).setSessionJsonState(
+      connectedSession.session,
+      ServerJson(
+        Seq(encoder(p1).deepDropNullValues)
+      )
+    )
